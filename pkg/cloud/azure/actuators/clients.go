c1202dca6a6536b757bb3215dc147df0d5222702
Stephen Augustus <saugustus@vmware.com>
2019-01-23 23:40:10 +0800

    Add Azure clients to actuator package
    Signed-off-by: Stephen Augustus <saugustus@vmware.com>

---
+++ pkg/cloud/azure/actuators/clients.go
@@ -0,0 +1,42 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package actuators
+
+import (
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2018-10-01/compute"
+	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2018-11-01/network"
+	"github.com/Azure/azure-sdk-for-go/services/resources/mgmt/2018-05-01/resources"
+)
+
+// AzureClients contains all the azure clients used by the scopes.
+type AzureClients struct {
+	// Compute
+	VM    compute.VirtualMachinesClient
+	Disks compute.DisksClient
+
+	// Network
+	VirtualNetworks   network.VirtualNetworksClient
+	SecurityGroups    network.SecurityGroupsClient
+	Interfaces        network.InterfacesClient
+	LB                network.LoadBalancersClient
+	PublicIPAddresses network.PublicIPAddressesClient
+
+	// Resources
+	Groups      resources.GroupsClient
+	Deployments resources.DeploymentsClient
+	Tags        resources.TagsClient
+}
