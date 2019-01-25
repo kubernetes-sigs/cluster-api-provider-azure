0cce31b334150f872dabd8917933190404b8b2f2
Stephen Augustus <saugustus@vmware.com>
2019-01-26 00:52:51 +0800

    Add boilerplate templates and scripts
    Signed-off-by: Stephen Augustus <saugustus@vmware.com>

--- hack/boilerplate/test/fail.go
+++ hack/boilerplate/test/fail.go
@@ -1,4 +1,7 @@
 /*
+Copyright 2014 The Kubernetes Authors.
+
+fail
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -12,3 +15,5 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
+package test
