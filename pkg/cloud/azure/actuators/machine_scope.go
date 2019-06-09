69ee9ba8624f05f510bccecb2137868156fea920
Stephen Augustus <saugustus@vmware.com>
2019-06-09 08:05:59 -0400

    Update scope and machine scope to use logr.Logger
    Signed-off-by: Stephen Augustus <saugustus@vmware.com>

--- pkg/cloud/azure/actuators/machine_scope.go
+++ pkg/cloud/azure/actuators/machine_scope.go
@@ -17,6 +17,9 @@ limitations under the License.
 package actuators
 
 import (
+	"fmt"
+
+	"github.com/go-logr/logr"
 	"github.com/pkg/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -33,17 +36,23 @@ type MachineScopeParams struct {
 	Cluster *clusterv1.Cluster
 	Machine *clusterv1.Machine
 	Client  client.ClusterV1alpha1Interface
+	Logger  logr.Logger
 }
 
 // NewMachineScope creates a new MachineScope from the supplied parameters.
 // This is meant to be called for each machine actuator operation.
 func NewMachineScope(params MachineScopeParams) (*MachineScope, error) {
-	scope, err := NewScope(ScopeParams{AzureClients: params.AzureClients, Client: params.Client, Cluster: params.Cluster})
+	scope, err := NewScope(ScopeParams{
+		AzureClients: params.AzureClients,
+		Client:       params.Client,
+		Cluster:      params.Cluster,
+		Logger:       params.Logger,
+	})
 	if err != nil {
 		return nil, err
 	}
 
-	machineConfig, err := MachineConfigFromProviderSpec(params.Client, params.Machine.Spec.ProviderSpec)
+	machineConfig, err := MachineConfigFromProviderSpec(params.Client, params.Machine.Spec.ProviderSpec, scope.Logger)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to get machine config")
 	}
@@ -137,43 +146,43 @@ func (m *MachineScope) Close() {
 }
 
 // MachineConfigFromProviderSpec tries to decode the JSON-encoded spec, falling back on getting a MachineClass if the value is absent.
-func MachineConfigFromProviderSpec(clusterClient client.MachineClassesGetter, providerConfig clusterv1.ProviderSpec) (*v1alpha1.AzureMachineProviderSpec, error) {
+func MachineConfigFromProviderSpec(clusterClient client.MachineClassesGetter, providerConfig clusterv1.ProviderSpec, log logr.Logger) (*v1alpha1.AzureMachineProviderSpec, error) {
 	var config v1alpha1.AzureMachineProviderSpec
 	if providerConfig.Value != nil {
-		klog.V(4).Info("Decoding ProviderConfig from Value")
-		return unmarshalProviderSpec(providerConfig.Value)
+		log.V(4).Info("Decoding ProviderConfig from Value")
+		return unmarshalProviderSpec(providerConfig.Value, log)
 	}
 
 	if providerConfig.ValueFrom != nil && providerConfig.ValueFrom.MachineClass != nil {
 		ref := providerConfig.ValueFrom.MachineClass
-		klog.V(4).Info("Decoding ProviderConfig from MachineClass")
-		klog.V(6).Infof("ref: %v", ref)
-		if ref.Provider != "" && ref.Provider != "azure" {
+		log.V(4).Info("Decoding ProviderConfig from MachineClass")
+		log.V(6).Info("Machine class reference", "ref", fmt.Sprintf("%+v", ref))
+		if ref.Provider != "" && ref.Provider != "aws" {
 			return nil, errors.Errorf("Unsupported provider: %q", ref.Provider)
 		}
 
 		if len(ref.Namespace) > 0 && len(ref.Name) > 0 {
-			klog.V(4).Infof("Getting MachineClass: %s/%s", ref.Namespace, ref.Name)
+			log.V(4).Info("Getting MachineClass", "reference-namespace", ref.Namespace, "reference-name", ref.Name)
 			mc, err := clusterClient.MachineClasses(ref.Namespace).Get(ref.Name, metav1.GetOptions{})
-			klog.V(6).Infof("Retrieved MachineClass: %+v", mc)
+			log.V(6).Info("Retrieved MachineClass", "machine-class", fmt.Sprintf("%+v", mc))
 			if err != nil {
 				return nil, err
 			}
 			providerConfig.Value = &mc.ProviderSpec
-			return unmarshalProviderSpec(&mc.ProviderSpec)
+			return unmarshalProviderSpec(&mc.ProviderSpec, log)
 		}
 	}
 
 	return &config, nil
 }
 
-func unmarshalProviderSpec(spec *runtime.RawExtension) (*v1alpha1.AzureMachineProviderSpec, error) {
+func unmarshalProviderSpec(spec *runtime.RawExtension, log logr.Logger) (*v1alpha1.AzureMachineProviderSpec, error) {
 	var config v1alpha1.AzureMachineProviderSpec
 	if spec != nil {
 		if err := yaml.Unmarshal(spec.Raw, &config); err != nil {
 			return nil, err
 		}
 	}
-	klog.V(6).Infof("Found ProviderSpec: %+v", config)
+	log.V(6).Info("Found ProviderSpec", "provider-spec", fmt.Sprintf("%+v", config))
 	return &config, nil
 }
