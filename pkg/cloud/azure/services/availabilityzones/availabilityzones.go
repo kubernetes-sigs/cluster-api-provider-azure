2b4e0a8763a3ba1dfadc379e7f3831b60561b39e
Ace Eldeib <alexeldeib@gmail.com>
2019-06-14 12:03:04 -0700

    fix: restriced locations and zones

--- pkg/cloud/azure/services/availabilityzones/availabilityzones.go
+++ pkg/cloud/azure/services/availabilityzones/availabilityzones.go
@@ -18,8 +18,10 @@ package availabilityzones
 
 import (
 	"context"
+	"sort"
 	"strings"
 
+	"github.com/Azure/azure-sdk-for-go/services/compute/mgmt/2019-03-01/compute"
 	"github.com/pkg/errors"
 	"sigs.k8s.io/cluster-api-provider-azure/pkg/apis/azureprovider/v1alpha1"
 )
@@ -45,9 +47,31 @@ func (s *Service) Get(ctx context.Context, spec v1alpha1.ResourceSpec) (interfac
 	for res.NotDone() {
 		resSku := res.Value()
 		if strings.EqualFold(*resSku.Name, skusSpec.VMSize) {
+			// Use map for easy deletion and iteration
+			availableZones := make(map[string]bool)
 			for _, locationInfo := range *resSku.LocationInfo {
+				for _, zone := range *locationInfo.Zones {
+					availableZones[zone] = true
+				}
 				if strings.EqualFold(*locationInfo.Location, s.Scope.ClusterConfig.Location) {
-					zones = *locationInfo.Zones
+					for _, restriction := range *resSku.Restrictions {
+						// Can't deploy anything in this subscription in this location. Bail out.
+						if restriction.Type == compute.Location {
+							return []string{}, errors.Errorf("rejecting sku: %s in location: %s due to susbcription restriction", skusSpec.VMSize, s.Scope.ClusteConfig.Location)
+						}
+						// May be able to deploy one or more zones to this location.
+						for _, restrictedZone := range *restriction.RestrictionInfo.Zones {
+							delete(availableZones, restrictedZone)
+						}
+					}
+					// Back to slice. Empty is fine, and will deploy the VM to some FD/UD (no point in configuring this until supported at higher levels)
+					result := make([]string, 0)
+					for availableZone := range availableZones {
+						result = append(result, availableZone)
+					}
+					// Lexical sort so comparisons work in tests
+					sort.Strings(result)
+					zones = result
 				}
 			}
 		}
