99fbe5700f95d6d55a4f60a53d6e2f4073280251
Stephen Augustus <justaugustus@users.noreply.github.com>
2019-07-19 19:46:55 -0400

    HA Clusters: phase 0 (#243)
    * Add/update machine templates and generate-yaml to support HA clusters
    
    Signed-off-by: Stephen Augustus <saugustus@vmware.com>
    
    * Update to Kubernetes 1.15.1
    
    Signed-off-by: Stephen Augustus <saugustus@vmware.com>

--- pkg/cloud/azure/actuators/machine/actuator_test.go
+++ pkg/cloud/azure/actuators/machine/actuator_test.go
@@ -80,8 +80,8 @@ func newMachine(t *testing.T, machineConfig v1alpha1.AzureMachineProviderSpec, l
 		Spec: clusterv1.MachineSpec{
 			ProviderSpec: *providerSpec,
 			Versions: clusterv1.MachineVersionInfo{
-				Kubelet:      "1.15.0",
-				ControlPlane: "1.15.0",
+				Kubelet:      "1.15.1",
+				ControlPlane: "1.15.1",
 			},
 		},
 	}
