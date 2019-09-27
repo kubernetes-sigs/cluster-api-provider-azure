ad92ce488728eb085d27b0d25539a2df4e28a4ae
Cecile Robert-Michon <cerobert@microsoft.com>
2019-09-27 15:30:35 -0700

    remove capi owner ref issue workaround

--- examples/generate.sh
+++ examples/generate.sh
@@ -123,9 +123,7 @@ kustomize build "${SOURCE_DIR}/machinedeployment" | envsubst >> "${MACHINEDEPLOY
 echo "Generated ${MACHINEDEPLOYMENT_GENERATED_FILE}"
 
 # Generate Cluster API provider components file.
-# kustomize build "github.com/kubernetes-sigs/cluster-api//config/default/?ref=v0.2.0" > "${COMPONENTS_CLUSTER_API_GENERATED_FILE}"
-# workaround for https://github.com/kubernetes-sigs/cluster-api/issues/1465
-wget https://github.com/kubernetes-sigs/cluster-api/releases/download/v0.2.3/cluster-api-components.yaml -O "${COMPONENTS_CLUSTER_API_GENERATED_FILE}"
+kustomize build "github.com/kubernetes-sigs/cluster-api//config/default/?ref=v0.2.0" > "${COMPONENTS_CLUSTER_API_GENERATED_FILE}"
 echo "Generated ${COMPONENTS_CLUSTER_API_GENERATED_FILE}"
 
 # Generate Kubeadm Bootstrap Provider components file.
