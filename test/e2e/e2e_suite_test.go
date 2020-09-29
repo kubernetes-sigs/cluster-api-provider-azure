2cc2b7bfab7cc661161cd3000a3a3f3d20994a40
Matt Boersma <Matt.Boersma@microsoft.com>
2020-09-29 13:51:44 -0600

    💚 collect workload cluster logs in e2e runs

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -128,7 +128,8 @@ var _ = SynchronizedBeforeSuite(func() []byte {
 	kubeconfigPath := parts[3]
 
 	e2eConfig = loadE2EConfig(configPath)
-	bootstrapClusterProxy = framework.NewClusterProxy("bootstrap", kubeconfigPath, initScheme())
+	bootstrapClusterProxy = framework.NewClusterProxy("bootstrap", kubeconfigPath, initScheme(),
+		framework.WithMachineLogCollector(AzureLogCollector{}))
 })
 
 // Using a SynchronizedAfterSuite for controlling how to delete resources shared across ParallelNodes (~ginkgo threads).
