202fa4e4f1ad1b16740874fd43efe808c3ea150b
shysank <sankarans@vmware.com>
2021-09-30 12:03:23 -0700

    add v1beta1 types

---
+++ exp/api/v1beta1/azuremachinepool_webhook.go
@@ -0,0 +1,194 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	kerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	infrav1 "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
+)
+
+// log is for logging in this package.
+var azuremachinepoollog = logf.Log.WithName("azuremachinepool-resource")
+
+// SetupWebhookWithManager sets up and registers the webhook with the manager.
+func (amp *AzureMachinePool) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(amp).
+		Complete()
+}
+
+// +kubebuilder:webhook:path=/mutate-infrastructure-cluster-x-k8s-io-v1beta1-azuremachinepool,mutating=true,failurePolicy=fail,groups=infrastructure.cluster.x-k8s.io,resources=azuremachinepools,verbs=create;update,versions=v1beta1,name=default.azuremachinepool.infrastructure.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta1
+
+var _ webhook.Defaulter = &AzureMachinePool{}
+
+// Default implements webhook.Defaulter so a webhook will be registered for the type.
+func (amp *AzureMachinePool) Default() {
+	azuremachinepoollog.Info("default", "name", amp.Name)
+
+	err := amp.SetDefaultSSHPublicKey()
+	if err != nil {
+		azuremachinepoollog.Error(err, "SetDefaultSshPublicKey failed")
+	}
+	amp.SetIdentityDefaults()
+}
+
+// +kubebuilder:webhook:verbs=create;update,path=/validate-infrastructure-cluster-x-k8s-io-v1beta1-azuremachinepool,mutating=false,failurePolicy=fail,groups=infrastructure.cluster.x-k8s.io,resources=azuremachinepools,versions=v1beta1,name=validation.azuremachinepool.infrastructure.cluster.x-k8s.io,sideEffects=None,admissionReviewVersions=v1;v1beta1
+
+var _ webhook.Validator = &AzureMachinePool{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type.
+func (amp *AzureMachinePool) ValidateCreate() error {
+	azuremachinepoollog.Info("validate create", "name", amp.Name)
+	return amp.Validate(nil)
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type.
+func (amp *AzureMachinePool) ValidateUpdate(old runtime.Object) error {
+	azuremachinepoollog.Info("validate update", "name", amp.Name)
+	return amp.Validate(old)
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type.
+func (amp *AzureMachinePool) ValidateDelete() error {
+	azuremachinepoollog.Info("validate delete", "name", amp.Name)
+	return nil
+}
+
+// Validate the Azure Machine Pool and return an aggregate error.
+func (amp *AzureMachinePool) Validate(old runtime.Object) error {
+	validators := []func() error{
+		amp.ValidateImage,
+		amp.ValidateTerminateNotificationTimeout,
+		amp.ValidateSSHKey,
+		amp.ValidateUserAssignedIdentity,
+		amp.ValidateStrategy(),
+		amp.ValidateSystemAssignedIdentity(old),
+	}
+
+	var errs []error
+	for _, validator := range validators {
+		if err := validator(); err != nil {
+			errs = append(errs, err)
+		}
+	}
+
+	return kerrors.NewAggregate(errs)
+}
+
+// ValidateImage of an AzureMachinePool.
+func (amp *AzureMachinePool) ValidateImage() error {
+	if amp.Spec.Template.Image != nil {
+		image := amp.Spec.Template.Image
+		if errs := infrav1.ValidateImage(image, field.NewPath("image")); len(errs) > 0 {
+			agg := kerrors.NewAggregate(errs.ToAggregate().Errors())
+			azuremachinepoollog.Info("Invalid image: %s", agg.Error())
+			return agg
+		}
+	}
+
+	return nil
+}
+
+// ValidateTerminateNotificationTimeout termination notification timeout to be between 5 and 15.
+func (amp *AzureMachinePool) ValidateTerminateNotificationTimeout() error {
+	if amp.Spec.Template.TerminateNotificationTimeout == nil {
+		return nil
+	}
+	if *amp.Spec.Template.TerminateNotificationTimeout < 5 {
+		return errors.New("Minimum timeout 5 is allowed for TerminateNotificationTimeout")
+	}
+
+	if *amp.Spec.Template.TerminateNotificationTimeout > 15 {
+		return errors.New("Maximum timeout 15 is allowed for TerminateNotificationTimeout")
+	}
+
+	return nil
+}
+
+// ValidateSSHKey validates an SSHKey.
+func (amp *AzureMachinePool) ValidateSSHKey() error {
+	if amp.Spec.Template.SSHPublicKey != "" {
+		sshKey := amp.Spec.Template.SSHPublicKey
+		if errs := infrav1.ValidateSSHKey(sshKey, field.NewPath("sshKey")); len(errs) > 0 {
+			agg := kerrors.NewAggregate(errs.ToAggregate().Errors())
+			azuremachinepoollog.Info("Invalid sshKey: %s", agg.Error())
+			return agg
+		}
+	}
+
+	return nil
+}
+
+// ValidateUserAssignedIdentity validates the user-assigned identities list.
+func (amp *AzureMachinePool) ValidateUserAssignedIdentity() error {
+	fldPath := field.NewPath("UserAssignedIdentities")
+	if errs := infrav1.ValidateUserAssignedIdentity(amp.Spec.Identity, amp.Spec.UserAssignedIdentities, fldPath); len(errs) > 0 {
+		return kerrors.NewAggregate(errs.ToAggregate().Errors())
+	}
+
+	return nil
+}
+
+// ValidateStrategy validates the strategy.
+func (amp *AzureMachinePool) ValidateStrategy() func() error {
+	return func() error {
+		if amp.Spec.Strategy.Type == RollingUpdateAzureMachinePoolDeploymentStrategyType && amp.Spec.Strategy.RollingUpdate != nil {
+			rollingUpdateStrategy := amp.Spec.Strategy.RollingUpdate
+			maxSurge := rollingUpdateStrategy.MaxSurge
+			maxUnavailable := rollingUpdateStrategy.MaxUnavailable
+			if maxSurge.Type == intstr.Int && maxSurge.IntVal == 0 &&
+				maxUnavailable.Type == intstr.Int && maxUnavailable.IntVal == 0 {
+				return errors.New("rolling update strategy MaxUnavailable must not be 0 if MaxSurge is 0")
+			}
+		}
+
+		return nil
+	}
+}
+
+// ValidateSystemAssignedIdentity validates system-assigned identity role.
+func (amp *AzureMachinePool) ValidateSystemAssignedIdentity(old runtime.Object) func() error {
+	return func() error {
+		var oldRole string
+		if old != nil {
+			oldMachinePool, ok := old.(*AzureMachinePool)
+			if !ok {
+				return fmt.Errorf("unexpected type for old azure machine pool object. Expected: %q, Got: %q",
+					"AzureMachinePool", reflect.TypeOf(old))
+			}
+			oldRole = oldMachinePool.Spec.RoleAssignmentName
+		}
+
+		fldPath := field.NewPath("roleAssignmentName")
+		if errs := infrav1.ValidateSystemAssignedIdentity(amp.Spec.Identity, oldRole, amp.Spec.RoleAssignmentName, fldPath); len(errs) > 0 {
+			return kerrors.NewAggregate(errs.ToAggregate().Errors())
+		}
+
+		return nil
+	}
+}
---
+++ exp/api/v1beta1/azuremachinepool_webhook_test.go
@@ -0,0 +1,366 @@
+/*
+Copyright 2021 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"encoding/base64"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"k8s.io/apimachinery/pkg/util/uuid"
+
+	"github.com/Azure/go-autorest/autorest/to"
+	. "github.com/onsi/gomega"
+	"golang.org/x/crypto/ssh"
+
+	guuid "github.com/google/uuid"
+
+	infrav1 "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
+)
+
+var (
+	validSSHPublicKey = generateSSHPublicKey(true)
+)
+
+func TestAzureMachinePool_ValidateCreate(t *testing.T) {
+	g := NewWithT(t)
+
+	var (
+		zero = intstr.FromInt(0)
+		one  = intstr.FromInt(1)
+	)
+
+	tests := []struct {
+		name    string
+		amp     *AzureMachinePool
+		wantErr bool
+	}{
+		{
+			name:    "azuremachinepool with marketplace image - full",
+			amp:     createMachinePoolWithtMarketPlaceImage("PUB1234", "OFFER1234", "SKU1234", "1.0.0", to.IntPtr(10)),
+			wantErr: false,
+		},
+		{
+			name:    "azuremachinepool with marketplace image - missing publisher",
+			amp:     createMachinePoolWithtMarketPlaceImage("", "OFFER1234", "SKU1234", "1.0.0", to.IntPtr(10)),
+			wantErr: true,
+		},
+		{
+			name:    "azuremachinepool with shared gallery image - full",
+			amp:     createMachinePoolWithSharedImage("SUB123", "RG123", "NAME123", "GALLERY1", "1.0.0", to.IntPtr(10)),
+			wantErr: false,
+		},
+		{
+			name:    "azuremachinepool with marketplace image - missing subscription",
+			amp:     createMachinePoolWithSharedImage("", "RG123", "NAME123", "GALLERY1", "1.0.0", to.IntPtr(10)),
+			wantErr: true,
+		},
+		{
+			name:    "azuremachinepool with image by - with id",
+			amp:     createMachinePoolWithImageByID("ID123", to.IntPtr(10)),
+			wantErr: false,
+		},
+		{
+			name:    "azuremachinepool with image by - without id",
+			amp:     createMachinePoolWithImageByID("", to.IntPtr(10)),
+			wantErr: true,
+		},
+		{
+			name:    "azuremachinepool with valid SSHPublicKey",
+			amp:     createMachinePoolWithSSHPublicKey(validSSHPublicKey),
+			wantErr: false,
+		},
+		{
+			name:    "azuremachinepool with invalid SSHPublicKey",
+			amp:     createMachinePoolWithSSHPublicKey("invalid ssh key"),
+			wantErr: true,
+		},
+		{
+			name:    "azuremachinepool with wrong terminate notification",
+			amp:     createMachinePoolWithSharedImage("SUB123", "RG123", "NAME123", "GALLERY1", "1.0.0", to.IntPtr(35)),
+			wantErr: true,
+		},
+		{
+			name:    "azuremachinepool with system assigned identity",
+			amp:     createMachinePoolWithSystemAssignedIdentity(string(uuid.NewUUID())),
+			wantErr: false,
+		},
+		{
+			name:    "azuremachinepool with system assigned identity, but invalid role",
+			amp:     createMachinePoolWithSystemAssignedIdentity("not_a_uuid"),
+			wantErr: true,
+		},
+		{
+			name:    "azuremachinepool with user assigned identity",
+			amp:     createMachinePoolWithUserAssignedIdentity([]string{"azure:://id1", "azure:://id2"}),
+			wantErr: false,
+		},
+		{
+			name:    "azuremachinepool with user assigned identity, but without any provider ids",
+			amp:     createMachinePoolWithUserAssignedIdentity([]string{}),
+			wantErr: true,
+		},
+		{
+			name: "azuremachinepool with invalid MaxSurge and MaxUnavailable rolling upgrade configuration",
+			amp: createMachinePoolWithStrategy(AzureMachinePoolDeploymentStrategy{
+				Type: RollingUpdateAzureMachinePoolDeploymentStrategyType,
+				RollingUpdate: &MachineRollingUpdateDeployment{
+					MaxSurge:       &zero,
+					MaxUnavailable: &zero,
+				},
+			}),
+			wantErr: true,
+		},
+		{
+			name: "azuremachinepool with valid MaxSurge and MaxUnavailable rolling upgrade configuration",
+			amp: createMachinePoolWithStrategy(AzureMachinePoolDeploymentStrategy{
+				Type: RollingUpdateAzureMachinePoolDeploymentStrategyType,
+				RollingUpdate: &MachineRollingUpdateDeployment{
+					MaxSurge:       &zero,
+					MaxUnavailable: &one,
+				},
+			}),
+			wantErr: false,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.amp.ValidateCreate()
+			if tc.wantErr {
+				g.Expect(err).To(HaveOccurred())
+			} else {
+				g.Expect(err).NotTo(HaveOccurred())
+			}
+		})
+	}
+}
+
+func TestAzureMachinePool_ValidateUpdate(t *testing.T) {
+	g := NewWithT(t)
+
+	var (
+		zero = intstr.FromInt(0)
+		one  = intstr.FromInt(1)
+	)
+
+	tests := []struct {
+		name    string
+		oldAMP  *AzureMachinePool
+		amp     *AzureMachinePool
+		wantErr bool
+	}{
+		{
+			name:    "azuremachinepool with valid SSHPublicKey",
+			oldAMP:  createMachinePoolWithSSHPublicKey(""),
+			amp:     createMachinePoolWithSSHPublicKey(validSSHPublicKey),
+			wantErr: false,
+		},
+		{
+			name:    "azuremachinepool with invalid SSHPublicKey",
+			oldAMP:  createMachinePoolWithSSHPublicKey(""),
+			amp:     createMachinePoolWithSSHPublicKey("invalid ssh key"),
+			wantErr: true,
+		},
+		{
+			name:    "azuremachinepool with system-assigned identity, and role unchanged",
+			oldAMP:  createMachinePoolWithSystemAssignedIdentity("30a757d8-fcf0-4c8b-acf0-9253a7e093ea"),
+			amp:     createMachinePoolWithSystemAssignedIdentity("30a757d8-fcf0-4c8b-acf0-9253a7e093ea"),
+			wantErr: false,
+		},
+		{
+			name:    "azuremachinepool with system-assigned identity, and role changed",
+			oldAMP:  createMachinePoolWithSystemAssignedIdentity(string(uuid.NewUUID())),
+			amp:     createMachinePoolWithSystemAssignedIdentity(string(uuid.NewUUID())),
+			wantErr: true,
+		},
+		{
+			name:   "azuremachinepool with invalid MaxSurge and MaxUnavailable rolling upgrade configuration",
+			oldAMP: createMachinePoolWithStrategy(AzureMachinePoolDeploymentStrategy{}),
+			amp: createMachinePoolWithStrategy(AzureMachinePoolDeploymentStrategy{
+				Type: RollingUpdateAzureMachinePoolDeploymentStrategyType,
+				RollingUpdate: &MachineRollingUpdateDeployment{
+					MaxSurge:       &zero,
+					MaxUnavailable: &zero,
+				},
+			}),
+			wantErr: true,
+		},
+		{
+			name:   "azuremachinepool with valid MaxSurge and MaxUnavailable rolling upgrade configuration",
+			oldAMP: createMachinePoolWithStrategy(AzureMachinePoolDeploymentStrategy{}),
+			amp: createMachinePoolWithStrategy(AzureMachinePoolDeploymentStrategy{
+				Type: RollingUpdateAzureMachinePoolDeploymentStrategyType,
+				RollingUpdate: &MachineRollingUpdateDeployment{
+					MaxSurge:       &zero,
+					MaxUnavailable: &one,
+				},
+			}),
+			wantErr: false,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.amp.ValidateUpdate(tc.oldAMP)
+			if tc.wantErr {
+				g.Expect(err).To(HaveOccurred())
+			} else {
+				g.Expect(err).NotTo(HaveOccurred())
+			}
+		})
+	}
+}
+
+func TestAzureMachinePool_Default(t *testing.T) {
+	g := NewWithT(t)
+
+	type test struct {
+		amp *AzureMachinePool
+	}
+
+	existingPublicKey := validSSHPublicKey
+	publicKeyExistTest := test{amp: createMachinePoolWithSSHPublicKey(existingPublicKey)}
+	publicKeyNotExistTest := test{amp: createMachinePoolWithSSHPublicKey("")}
+
+	existingRoleAssignmentName := "42862306-e485-4319-9bf0-35dbc6f6fe9c"
+	roleAssignmentExistTest := test{amp: &AzureMachinePool{Spec: AzureMachinePoolSpec{
+		Identity:           "SystemAssigned",
+		RoleAssignmentName: existingRoleAssignmentName,
+	}}}
+
+	roleAssignmentEmptyTest := test{amp: &AzureMachinePool{Spec: AzureMachinePoolSpec{
+		Identity:           "SystemAssigned",
+		RoleAssignmentName: "",
+	}}}
+
+	roleAssignmentExistTest.amp.Default()
+	g.Expect(roleAssignmentExistTest.amp.Spec.RoleAssignmentName).To(Equal(existingRoleAssignmentName))
+
+	roleAssignmentEmptyTest.amp.Default()
+	g.Expect(roleAssignmentEmptyTest.amp.Spec.RoleAssignmentName).To(Not(BeEmpty()))
+	_, err := guuid.Parse(roleAssignmentEmptyTest.amp.Spec.RoleAssignmentName)
+	g.Expect(err).To(Not(HaveOccurred()))
+
+	publicKeyExistTest.amp.Default()
+	g.Expect(publicKeyExistTest.amp.Spec.Template.SSHPublicKey).To(Equal(existingPublicKey))
+
+	publicKeyNotExistTest.amp.Default()
+	g.Expect(publicKeyNotExistTest.amp.Spec.Template.SSHPublicKey).NotTo(BeEmpty())
+}
+
+func createMachinePoolWithtMarketPlaceImage(publisher, offer, sku, version string, terminateNotificationTimeout *int) *AzureMachinePool {
+	image := infrav1.Image{
+		Marketplace: &infrav1.AzureMarketplaceImage{
+			Publisher: publisher,
+			Offer:     offer,
+			SKU:       sku,
+			Version:   version,
+		},
+	}
+
+	return &AzureMachinePool{
+		Spec: AzureMachinePoolSpec{
+			Template: AzureMachinePoolMachineTemplate{
+				Image:                        &image,
+				SSHPublicKey:                 validSSHPublicKey,
+				TerminateNotificationTimeout: terminateNotificationTimeout,
+			},
+		},
+	}
+}
+
+func createMachinePoolWithSharedImage(subscriptionID, resourceGroup, name, gallery, version string, terminateNotificationTimeout *int) *AzureMachinePool {
+	image := infrav1.Image{
+		SharedGallery: &infrav1.AzureSharedGalleryImage{
+			SubscriptionID: subscriptionID,
+			ResourceGroup:  resourceGroup,
+			Name:           name,
+			Gallery:        gallery,
+			Version:        version,
+		},
+	}
+
+	return &AzureMachinePool{
+		Spec: AzureMachinePoolSpec{
+			Template: AzureMachinePoolMachineTemplate{
+				Image:                        &image,
+				SSHPublicKey:                 validSSHPublicKey,
+				TerminateNotificationTimeout: terminateNotificationTimeout,
+			},
+		},
+	}
+}
+
+func createMachinePoolWithImageByID(imageID string, terminateNotificationTimeout *int) *AzureMachinePool {
+	image := infrav1.Image{
+		ID: &imageID,
+	}
+
+	return &AzureMachinePool{
+		Spec: AzureMachinePoolSpec{
+			Template: AzureMachinePoolMachineTemplate{
+				Image:                        &image,
+				SSHPublicKey:                 validSSHPublicKey,
+				TerminateNotificationTimeout: terminateNotificationTimeout,
+			},
+		},
+	}
+}
+
+func createMachinePoolWithSystemAssignedIdentity(role string) *AzureMachinePool {
+	return &AzureMachinePool{
+		Spec: AzureMachinePoolSpec{
+			Identity:           infrav1.VMIdentitySystemAssigned,
+			RoleAssignmentName: role,
+		},
+	}
+}
+
+func createMachinePoolWithUserAssignedIdentity(providerIds []string) *AzureMachinePool {
+	userAssignedIdentities := make([]infrav1.UserAssignedIdentity, len(providerIds))
+
+	for _, providerID := range providerIds {
+		userAssignedIdentities = append(userAssignedIdentities, infrav1.UserAssignedIdentity{
+			ProviderID: providerID,
+		})
+	}
+
+	return &AzureMachinePool{
+		Spec: AzureMachinePoolSpec{
+			Identity:               infrav1.VMIdentityUserAssigned,
+			UserAssignedIdentities: userAssignedIdentities,
+		},
+	}
+}
+
+func generateSSHPublicKey(b64Enconded bool) string {
+	privateKey, _ := rsa.GenerateKey(rand.Reader, 2048)
+	publicRsaKey, _ := ssh.NewPublicKey(&privateKey.PublicKey)
+	if b64Enconded {
+		return base64.StdEncoding.EncodeToString(ssh.MarshalAuthorizedKey(publicRsaKey))
+	}
+	return string(ssh.MarshalAuthorizedKey(publicRsaKey))
+}
+
+func createMachinePoolWithStrategy(strategy AzureMachinePoolDeploymentStrategy) *AzureMachinePool {
+	return &AzureMachinePool{
+		Spec: AzureMachinePoolSpec{
+			Strategy: strategy,
+		},
+	}
+}
