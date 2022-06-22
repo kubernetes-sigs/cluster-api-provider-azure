9afbebeabd2a26fd701dd6b50e0a3bd2dfff6fc0
Brian Lieberman <brianlieberman@users.noreply.github.com>
2022-06-22 16:09:27 -0400

    Update machinepool_test.go

--- azure/scope/machinepool_test.go
+++ azure/scope/machinepool_test.go
@@ -166,7 +166,7 @@ func TestMachinePoolScope_SetBootstrapConditions(t *testing.T) {
 				g.Expect(conditions.IsFalse(amp, infrav1.BootstrapSucceededCondition))
 				g.Expect(conditions.GetReason(amp, infrav1.BootstrapSucceededCondition)).To(Equal(infrav1.BootstrapInProgressReason))
 				severity := conditions.GetSeverity(amp, infrav1.BootstrapSucceededCondition)
-				g.Expect(severity).ToNot(BeNil())
+				g.Expect(severity).NotTo(BeNil())
 				g.Expect(*severity).To(Equal(clusterv1.ConditionSeverityInfo))
 			},
 		},
@@ -180,7 +180,7 @@ func TestMachinePoolScope_SetBootstrapConditions(t *testing.T) {
 				g.Expect(conditions.IsFalse(amp, infrav1.BootstrapSucceededCondition))
 				g.Expect(conditions.GetReason(amp, infrav1.BootstrapSucceededCondition)).To(Equal(infrav1.BootstrapFailedReason))
 				severity := conditions.GetSeverity(amp, infrav1.BootstrapSucceededCondition)
-				g.Expect(severity).ToNot(BeNil())
+				g.Expect(severity).NotTo(BeNil())
 				g.Expect(*severity).To(Equal(clusterv1.ConditionSeverityError))
 			},
 		},
