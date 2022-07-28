d3ddc6aaa7edc6b0938d866405a67eba7c7a2c13
Lan Lou <1554893443@qq.com>
2022-07-28 10:32:04 +0000

    Stable windows CNM creating process

--- scripts/ci-entrypoint.sh
+++ scripts/ci-entrypoint.sh
@@ -181,6 +181,10 @@ timeout --foreground 1800 bash -c wait_for_nodes
 
 # install cloud-provider-azure components, if using out-of-tree
 if [[ -n "${TEST_CCM:-}" ]]; then
+    if [[ -n "${TEST_WINDOWS:-}" ]]; then
+        # "app=calico" is the label only for calico-node-windows pods
+        "${KUBECTL}" wait --for=condition=Ready pod -l app=calico -n kube-system --timeout=10m
+    fi
     echo "Installing cloud-provider-azure components via helm"
     "${HELM}" install --repo https://raw.githubusercontent.com/kubernetes-sigs/cloud-provider-azure/master/helm/repo cloud-provider-azure --generate-name \
 --set infra.clusterName="${CLUSTER_NAME}" \
@@ -191,6 +195,8 @@ if [[ -n "${TEST_CCM:-}" ]]; then
 --set-string cloudControllerManager.imageTag="${IMAGE_TAG}" \
 --set-string cloudNodeManager.imageTag="${IMAGE_TAG}" \
 --set cloudControllerManager.replicas="${CCM_COUNT}"
+    echo "Waiting for all kube-system pods to be ready"
+    "${KUBECTL}" wait --for=condition=Ready pod -n kube-system --all --timeout=10m
 fi
 
 if [[ "${#}" -gt 0 ]]; then
