86c7f8e4c486f4848c0bab065d63e1cb44275556
Jonathan Tong <jt572@cornell.edu>
2022-08-10 14:45:46 -0400

    Watch Cluster for changes in AzureJSONMachineTemplate, AzureJSONMachine, and AzureJSONMachinePool controllers

--- controllers/azurejson_machinepool_controller.go
+++ controllers/azurejson_machinepool_controller.go
@@ -33,12 +33,15 @@ import (
 	expv1 "sigs.k8s.io/cluster-api-provider-azure/exp/api/v1beta1"
 	"sigs.k8s.io/cluster-api-provider-azure/util/reconciler"
 	"sigs.k8s.io/cluster-api-provider-azure/util/tele"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 	"sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/cluster-api/util/predicates"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
 )
 
 // AzureJSONMachinePoolReconciler reconciles Azure json secrets for AzureMachinePool objects.
@@ -56,12 +59,34 @@ func (r *AzureJSONMachinePoolReconciler) SetupWithManager(ctx context.Context, m
 	)
 	defer done()
 
-	return ctrl.NewControllerManagedBy(mgr).
+	azureMachinePoolMapper, err := util.ClusterToObjectsMapper(r.Client, &expv1.AzureMachinePoolList{}, mgr.GetScheme())
+	if err != nil {
+		return errors.Wrap(err, "failed to create mapper for Cluster to AzureMachinePools")
+	}
+
+	c, err := ctrl.NewControllerManagedBy(mgr).
 		WithOptions(options).
 		For(&expv1.AzureMachinePool{}).
 		WithEventFilter(predicates.ResourceNotPausedAndHasFilterLabel(log, r.WatchFilterValue)).
 		Owns(&corev1.Secret{}).
-		Complete(r)
+		Build(r)
+
+	if err != nil {
+		return errors.Wrap(err, "failed to create controller")
+	}
+
+	// Add a watch on Clusters to requeue when the infraRef is set. This is needed because the infraRef is not initially
+	// set in Clusters created from a ClusterClass.
+	if err := c.Watch(
+		&source.Kind{Type: &clusterv1.Cluster{}},
+		handler.EnqueueRequestsFromMapFunc(azureMachinePoolMapper),
+		predicates.ClusterUnpausedAndInfrastructureReady(log),
+		predicates.ResourceNotPausedAndHasFilterLabel(log, r.WatchFilterValue),
+	); err != nil {
+		return errors.Wrap(err, "failed adding a watch for Clusters")
+	}
+
+	return nil
 }
 
 // Reconcile reconciles the Azure json for AzureMachinePool objects.
