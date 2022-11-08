e24d1361138cb83ca0b7190eab3a6fd9be35ee5f
Matt Boersma <Matt.Boersma@microsoft.com>
2022-11-08 12:55:06 -0700

    Silence kubectl deprecation warning

--- scripts/ci-configmap.sh
+++ scripts/ci-configmap.sh
@@ -25,5 +25,5 @@ make --directory="${REPO_ROOT}" "${KUBECTL##*/}"
 CM_NAMES=("calico-addon" "calico-ipv6-addon" "calico-dual-stack-addon" "calico-windows-addon")
 CM_FILES=("calico.yaml" "calico-ipv6.yaml" "calico-dual-stack.yaml" "windows/calico")
 for i in "${!CM_NAMES[@]}"; do
-	"${KUBECTL}" create configmap "${CM_NAMES[i]}" --from-file="${REPO_ROOT}/templates/addons/${CM_FILES[i]}" --dry-run -o yaml | kubectl apply -f -
+	"${KUBECTL}" create configmap "${CM_NAMES[i]}" --from-file="${REPO_ROOT}/templates/addons/${CM_FILES[i]}" --dry-run=client -o yaml | kubectl apply -f -
 done
