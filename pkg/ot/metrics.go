afc5f5af00a224c5618ff349c5590985e9897675
Matt Boersma <Matt.Boersma@microsoft.com>
2023-01-18 13:02:06 -0700

    Update opentelemetry-go to v1.11.2 (#3039)

--- pkg/ot/metrics.go
+++ pkg/ot/metrics.go
@@ -20,33 +20,20 @@ import (
 	crprometheus "github.com/prometheus/client_golang/prometheus"
 	"go.opentelemetry.io/otel/exporters/prometheus"
 	"go.opentelemetry.io/otel/metric/global"
-	"go.opentelemetry.io/otel/sdk/metric/aggregator/histogram"
-	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
-	"go.opentelemetry.io/otel/sdk/metric/export/aggregation"
-	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
-	selector "go.opentelemetry.io/otel/sdk/metric/selector/simple"
+	"go.opentelemetry.io/otel/sdk/metric"
 	"sigs.k8s.io/controller-runtime/pkg/metrics"
 )
 
 // RegisterMetrics enables prometheus metrics for OpenTelemetry.
 func RegisterMetrics() error {
-	config := prometheus.Config{
-		Registry: metrics.Registry.(*crprometheus.Registry), // use the controller runtime metrics registry / gatherer
-	}
-	c := controller.New(
-		processor.NewFactory(
-			selector.NewWithHistogramDistribution(
-				histogram.WithExplicitBoundaries(config.DefaultHistogramBoundaries),
-			),
-			aggregation.CumulativeTemporalitySelector(),
-			processor.WithMemory(true),
-		),
+	exporter, err := prometheus.New(
+		prometheus.WithRegisterer(metrics.Registry.(*crprometheus.Registry)),
 	)
-	exporter, err := prometheus.New(config, c)
 	if err != nil {
 		return err
 	}
-	global.SetMeterProvider(exporter.MeterProvider())
+	meterProvider := metric.NewMeterProvider(metric.WithReader(exporter))
+	global.SetMeterProvider(meterProvider)
 
 	return nil
 }
