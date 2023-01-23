kubernetes-sigs/cluster-api-provider-azure

commit cd4342acaf97e6596d807332b886a541b75ae1a2

Support Tilt deployment of multiple workload clusters of the same flavor

Tiltfile
@@ -250,6 +250,12 @@ def flavors():
     for template in worker_templates:
         deploy_worker_templates(template, substitutions)
 
+    local_resource(
+        name = 'delete-all-workload-clusters',
+        cmd = "kubectl delete clusters --all --wait=false",
+        auto_init = False,
+        trigger_mode = TRIGGER_MODE_MANUAL
+    )
 
 def deploy_worker_templates(template, substitutions):
     # validate template exists
@@ -285,10 +291,9 @@ def deploy_worker_templates(template, substitutions):
     # programmatically define any remaining vars
     # "windows" can not be for cluster name because it sets the dns to trademarked name during reconciliation
     substitutions = {
-        "CLUSTER_NAME": flavor.replace("windows", "win") + "-template",
         "AZURE_LOCATION": "eastus",
-        "AZURE_VNET_NAME": flavor + "-template-vnet",
-        "AZURE_RESOURCE_GROUP": flavor + "-template-rg",
+        "AZURE_VNET_NAME": "${CLUSTER_NAME}-vnet",
+        "AZURE_RESOURCE_GROUP": "${CLUSTER_NAME}-rg",
         "CONTROL_PLANE_MACHINE_COUNT": "1",
         "KUBERNETES_VERSION": settings.get("kubernetes_version"),
         "AZURE_CONTROL_PLANE_MACHINE_TYPE": "Standard_D2s_v3",
@@ -305,10 +310,9 @@ def deploy_worker_templates(template, substitutions):
         yaml = yaml.replace("${" + substitution + "}", value)
 
     yaml = yaml.replace('"', '\\"')     # add escape character to double quotes in yaml
-
     local_resource(
-        os.path.basename(flavor),
-        cmd = "make generate-flavors; echo \"" + yaml + "\" > ./.tiltbuild/" + flavor + "; cat ./.tiltbuild/" + flavor + " | " + envsubst_cmd + " | kubectl apply -f -",
+        name = os.path.basename(flavor),
+        cmd = "CLUSTER_NAME=" + flavor.replace("windows", "win") + "-$(echo $RANDOM); make generate-flavors; echo \"" + yaml + "\" > ./.tiltbuild/" + flavor + "; cat ./.tiltbuild/" + flavor + " | " + envsubst_cmd + " | kubectl apply -f - && echo \"Cluster \'$CLUSTER_NAME\' created, don't forget to delete\"",
         auto_init = False,
         trigger_mode = TRIGGER_MODE_MANUAL
     )

.gitignore
@@ -37,6 +37,7 @@ kubeconfig
 
 # vscode
 .vscode
+*.code-workspace
 
 # goland
 .idea

commit e77349e8a6a61f07739e6baea54d194e851741c6

Tiltfile: fix templating cluster name
I'm not even sure how this could work previously, as envsubst works only
with environment variables, not with shell variables.

Perhaps cd4342acaf97e6596d807332b886a541b75ae1a2 broke it by moving
CLUSTER_NAME from Tilt-based substitutions to envsubst.

In any case, exporting CLUSTER_NAME makes envsubst properly populate
resource names, which fixes the problem.

Signed-off-by: Mateusz Gozdek <mgozdek@microsoft.com>

Tiltfile
@@ -349,7 +349,7 @@ def deploy_worker_templates(template, substitutions):
 
     yaml = shlex.quote(yaml)
     flavor_name = os.path.basename(flavor)
-    flavor_cmd = "RANDOM=$(bash -c 'echo $RANDOM'); CLUSTER_NAME=" + flavor.replace("windows", "win") + "-$RANDOM; make generate-flavors; echo " + yaml + "> ./.tiltbuild/" + flavor + "; cat ./.tiltbuild/" + flavor + " | " + envsubst_cmd + " | " + kubectl_cmd + " apply -f - && echo \"Cluster \'$CLUSTER_NAME\' created, don't forget to delete\""
+    flavor_cmd = "RANDOM=$(bash -c 'echo $RANDOM'); export CLUSTER_NAME=" + flavor.replace("windows", "win") + "-$RANDOM; make generate-flavors; echo " + yaml + "> ./.tiltbuild/" + flavor + "; cat ./.tiltbuild/" + flavor + " | " + envsubst_cmd + " | " + kubectl_cmd + " apply -f - && echo \"Cluster \'$CLUSTER_NAME\' created, don't forget to delete\""
 
     # wait for kubeconfig to be available
     flavor_cmd += "; until " + kubectl_cmd + " get secret ${CLUSTER_NAME}-kubeconfig > /dev/null 2>&1; do sleep 5; done; " + kubectl_cmd + " get secret ${CLUSTER_NAME}-kubeconfig -o jsonpath={.data.value} | base64 --decode > ./${CLUSTER_NAME}.kubeconfig; chmod 600 ./${CLUSTER_NAME}.kubeconfig; until " + kubectl_cmd + " --kubeconfig=./${CLUSTER_NAME}.kubeconfig get nodes > /dev/null 2>&1; do sleep 5; done"