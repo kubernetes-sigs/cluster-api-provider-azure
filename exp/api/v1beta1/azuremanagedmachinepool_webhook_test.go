4a05a807021c606d9bb9813e46f006e54a2430ab
Jon Huhn <johuhn@microsoft.com>
2023-03-23 14:34:23 -0500

    add missing create verb to AzureManagedCluster, AzureManagedMachinePool webhooks

--- exp/api/v1beta1/azuremanagedmachinepool_webhook_test.go
+++ exp/api/v1beta1/azuremanagedmachinepool_webhook_test.go
@@ -619,10 +619,8 @@ func TestAzureManagedMachinePool_ValidateCreate(t *testing.T) {
 		{
 			name: "Windows clusters with more than 6char names are not allowed",
 			ammp: &AzureManagedMachinePool{
-				ObjectMeta: metav1.ObjectMeta{
-					Name: "pool0-name-too-long",
-				},
 				Spec: AzureManagedMachinePoolSpec{
+					Name:   to.StringPtr("pool0-name-too-long"),
 					Mode:   "User",
 					OSType: to.StringPtr(azure.WindowsOS),
 				},
