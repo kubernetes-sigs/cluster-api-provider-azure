2e26b5c0c124b0cf6127cf6847eb3b0f6af42c96
swastik959 <Sswastik959@gmail.com>
2023-04-08 19:00:17 +0530

    refracted to gomega

--- azure/converters/rules_test.go
+++ azure/converters/rules_test.go
@@ -17,47 +17,49 @@ import (
 	"testing"
 
 	"github.com/Azure/azure-sdk-for-go/services/network/mgmt/2021-08-01/network"
-	"github.com/stretchr/testify/assert"
+	"github.com/onsi/gomega"
 	"k8s.io/utils/pointer"
 	infrav1 "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 )
 
 func TestSecurityRuleToSDK(t *testing.T) {
+	g := gomega.NewWithT(t)
+
 	// Test case for a security rule with all fields provided
 	rule := infrav1.SecurityRule{
-		Name: "test-rule",
-		Description: "Test security rule",
-		Source: "10.0.0.0/24",
-		SourcePorts: "80,443",
-		Destination: "192.168.0.0/16",
+		Name:             "test-rule",
+		Description:      "Test security rule",
+		Source:           "10.0.0.0/24",
+		SourcePorts:      "80,443",
+		Destination:      "192.168.0.0/16",
 		DestinationPorts: "22",
-		Protocol: infrav1.SecurityGroupProtocolTCP,
-		Direction: infrav1.SecurityRuleDirectionInbound,
-		Priority: 100,
+		Protocol:         infrav1.SecurityGroupProtocolTCP,
+		Direction:        infrav1.SecurityRuleDirectionInbound,
+		Priority:         100,
 	}
 
 	expectedResult := network.SecurityRule{
 		Name: pointer.String("test-rule"),
 		SecurityRulePropertiesFormat: &network.SecurityRulePropertiesFormat{
-			Description: pointer.String("Test security rule"),
-			SourceAddressPrefix: pointer.String("10.0.0.0/24"),
-			SourcePortRange: pointer.String("80,443"),
+			Description:              pointer.String("Test security rule"),
+			SourceAddressPrefix:      pointer.String("10.0.0.0/24"),
+			SourcePortRange:          pointer.String("80,443"),
 			DestinationAddressPrefix: pointer.String("192.168.0.0/16"),
-			DestinationPortRange: pointer.String("22"),
-			Access: network.SecurityRuleAccessAllow,
-			Priority: pointer.Int32(100),
+			DestinationPortRange:     pointer.String("22"),
+			Access:                   network.SecurityRuleAccessAllow,
+			Priority:                 pointer.Int32(100),
 		},
-		Protocol: network.SecurityRuleProtocolTCP,
+		Protocol:  network.SecurityRuleProtocolTCP,
 		Direction: network.SecurityRuleDirectionInbound,
 	}
 
 	actualResult := SecurityRuleToSDK(rule)
-	assert.Equal(t, expectedResult, actualResult)
+	g.Expect(actualResult).To(gomega.Equal(expectedResult))
 
 	// Test case for a security rule with minimum fields provided
 	rule = infrav1.SecurityRule{
-		Name: "test-rule",
-		Protocol: infrav1.SecurityGroupProtocolAll,
+		Name:      "test-rule",
+		Protocol:  infrav1.SecurityGroupProtocolAll,
 		Direction: infrav1.SecurityRuleDirectionInbound,
 	}
 
@@ -66,28 +68,28 @@ func TestSecurityRuleToSDK(t *testing.T) {
 		SecurityRulePropertiesFormat: &network.SecurityRulePropertiesFormat{
 			Access: network.SecurityRuleAccessAllow,
 		},
-		Protocol: network.SecurityRuleProtocolAsterisk,
+		Protocol:  network.SecurityRuleProtocolAsterisk,
 		Direction: network.SecurityRuleDirectionInbound,
 	}
 
 	actualResult = SecurityRuleToSDK(rule)
-	assert.Equal(t, expectedResult, actualResult)
+	g.Expect(actualResult).To(gomega.Equal(expectedResult))
 
 	// Test case for a security rule with invalid protocol
 	rule = infrav1.SecurityRule{
-		Name: "test-rule",
-		Protocol: "invalid-protocol",
+		Name:      "test-rule",
+		Protocol:  "invalid-protocol",
 		Direction: infrav1.SecurityRuleDirectionInbound,
 	}
 
-	assert.PanicsWithError(t, "invalid protocol value", func() { SecurityRuleToSDK(rule) })
+	g.Expect(func() { SecurityRuleToSDK(rule) }).To(gomega.PanicWith("invalid protocol value"))
 
 	// Test case for a security rule with invalid direction
 	rule = infrav1.SecurityRule{
-		Name: "test-rule",
-		Protocol: infrav1.SecurityGroupProtocolAll,
+		Name:      "test-rule",
+		Protocol:  infrav1.SecurityGroupProtocolAll,
 		Direction: "invalid-direction",
 	}
 
-	assert.PanicsWithError(t, "invalid direction value", func() { SecurityRuleToSDK(rule) })
+	g.Expect(func() { SecurityRuleToSDK(rule) }).To(gomega.PanicWith("invalid direction value"))
 }
