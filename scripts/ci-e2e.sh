20a01b3326b6c0aefce3cbd10f8fd96d54b4325e
Mark Rossetti <marosset@microsoft.com>
2023-07-06 14:13:00 -0700

    renaming cleanup function in ci-build-azure-ccm.sh and ci-build-kubernetes.sh
    Signed-off-by: Mark Rossetti <marosset@microsoft.com>

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -75,11 +75,11 @@ export KIND_EXPERIMENTAL_DOCKER_NETWORK="bridge"
 
 capz::util::generate_ssh_key
 
-cleanup() {
+capz::ci-e2e::cleanup() {
     "${REPO_ROOT}/hack/log/redact.sh" || true
 }
 
-trap cleanup EXIT
+trap capz::ci-e2e::cleanup EXIT
 # Image is configured as `${CONTROLLER_IMG}-${ARCH}:${TAG}` where `CONTROLLER_IMG` is defaulted to `${REGISTRY}/${IMAGE_NAME}`.
 if [[ "${BUILD_MANAGER_IMAGE}" == "false" ]]; then
   # Load an existing image, skip docker-build and docker-push.
