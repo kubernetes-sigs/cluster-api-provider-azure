f4a09283e7a624f113cf0ff49f3d1b9406f6d79e
willie-yao <williamyao@microsoft.com>
2023-10-13 22:07:34 +0000

    Convert bastionhosts service to ASO

--- azure/services/bastionhosts/spec_test.go
+++ azure/services/bastionhosts/spec_test.go
@@ -18,23 +18,51 @@ package bastionhosts
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork/v4"
+	asonetworkv1 "github.com/Azure/azure-service-operator/v2/api/network/v1api20220701"
+	"github.com/Azure/azure-service-operator/v2/pkg/genruntime"
 	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
 	infrav1 "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
 )
 
 var (
-	fakeSKU         = armnetwork.BastionHostSKUName("fake-SKU")
-	fakeBastionHost = armnetwork.BastionHost{
-		Location: &fakeAzureBastionSpec.Location,
-		Name:     ptr.To("my-bastion-host"),
-		SKU:      &armnetwork.SKU{Name: &fakeSKU},
+	fakeSKU         = asonetworkv1.Sku_Name("fake-SKU")
+	fakeBastionHost = asonetworkv1.BastionHost{
+		Spec: asonetworkv1.BastionHost_Spec{
+			Location:  ptr.To(fakeAzureBastionSpec1.Location),
+			AzureName: fakeAzureBastionSpec1.Name,
+			DnsName:   ptr.To(fakeAzureBastionSpec1.Name + "-bastion"),
+			Sku:       &asonetworkv1.Sku{Name: &fakeSKU},
+			Owner: &genruntime.KnownResourceReference{
+				Name: fakeAzureBastionSpec1.ResourceGroup,
+			},
+			Tags:            fakeBastionHostTags,
+			EnableTunneling: ptr.To(false),
+			IpConfigurations: []asonetworkv1.BastionHostIPConfiguration{
+				{
+					Name: ptr.To(fmt.Sprintf("%s-%s", fakeAzureBastionSpec1.Name, "bastionIP")),
+					Subnet: &asonetworkv1.BastionHostSubResource{
+						Reference: &genruntime.ResourceReference{
+							ARMID: fakeAzureBastionSpec1.SubnetID,
+						},
+					},
+					PublicIPAddress: &asonetworkv1.BastionHostSubResource{
+						Reference: &genruntime.ResourceReference{
+							ARMID: fakeAzureBastionSpec1.PublicIPID,
+						},
+					},
+					PrivateIPAllocationMethod: ptr.To(asonetworkv1.IPAllocationMethod_Dynamic),
+				},
+			},
+		},
 	}
 	fakeAzureBastionSpec1 = AzureBastionSpec{
-		Name:            "my-bastion",
+		Name:            "my-bastion-host",
+		Namespace:       "default",
 		ClusterName:     "cluster",
 		Location:        "westus",
 		SubnetID:        "my-subnet-id",
@@ -43,64 +71,118 @@ var (
 		EnableTunneling: false,
 	}
 
-	fakeBastionHostTags = map[string]*string{
-		"sigs.k8s.io_cluster-api-provider-azure_cluster_cluster": ptr.To("owned"),
-		"sigs.k8s.io_cluster-api-provider-azure_role":            ptr.To("Bastion"),
-		"Name": ptr.To("my-bastion"),
+	fakeBastionHostStatus = asonetworkv1.BastionHost_STATUS{
+		Name:              ptr.To(fakeAzureBastionSpec1.Name),
+		ProvisioningState: ptr.To(asonetworkv1.BastionHostProvisioningState_STATUS_Succeeded),
+	}
+
+	fakeBastionHostTags = map[string]string{
+		"sigs.k8s.io_cluster-api-provider-azure_cluster_cluster": "owned",
+		"sigs.k8s.io_cluster-api-provider-azure_role":            "Bastion",
+		"Name": fakeAzureBastionSpec1.Name,
 	}
 )
 
+func getASOBastionHost(changes ...func(*asonetworkv1.BastionHost)) *asonetworkv1.BastionHost {
+	BastionHost := fakeBastionHost.DeepCopy()
+	for _, change := range changes {
+		change(BastionHost)
+	}
+	return BastionHost
+}
+
 func TestAzureBastionSpec_Parameters(t *testing.T) {
-	testCases := []struct {
+	testcases := []struct {
 		name          string
 		spec          *AzureBastionSpec
-		existing      interface{}
-		expect        func(g *WithT, result interface{})
+		existing      *asonetworkv1.BastionHost
+		expect        func(g *WithT, result asonetworkv1.BastionHost)
 		expectedError string
 	}{
 		{
-			name:     "error when existing host is not of BastionHost type",
-			spec:     &AzureBastionSpec{},
-			existing: struct{}{},
-			expect: func(g *WithT, result interface{}) {
-				g.Expect(result).To(BeNil())
-			},
-			expectedError: "struct {} is not an armnetwork.BastionHost",
-		},
-		{
-			name:     "get result as nil when existing BastionHost is present",
-			spec:     &AzureBastionSpec{},
-			existing: fakeBastionHost,
-			expect: func(g *WithT, result interface{}) {
-				g.Expect(result).To(BeNil())
-			},
-			expectedError: "",
-		},
-		{
-			name:     "get result as nil when existing BastionHost is present with empty data",
-			spec:     &AzureBastionSpec{},
-			existing: armnetwork.BastionHost{},
-			expect: func(g *WithT, result interface{}) {
-				g.Expect(result).To(BeNil())
-			},
-			expectedError: "",
-		},
-		{
-			name:     "get BastionHost when all values are present",
+			name:     "Creating a new BastionHost",
 			spec:     &fakeAzureBastionSpec1,
 			existing: nil,
-			expect: func(g *WithT, result interface{}) {
-				g.Expect(result).To(BeAssignableToTypeOf(armnetwork.BastionHost{}))
-				g.Expect(result.(armnetwork.BastionHost).Location).To(Equal(ptr.To[string](fakeAzureBastionSpec1.Location)))
-				g.Expect(result.(armnetwork.BastionHost).Name).To(Equal(ptr.To[string](fakeAzureBastionSpec1.ResourceName())))
-				g.Expect(result.(armnetwork.BastionHost).SKU.Name).To(Equal(ptr.To(armnetwork.BastionHostSKUName(fakeAzureBastionSpec1.Sku))))
-				g.Expect(result.(armnetwork.BastionHost).Properties.EnableTunneling).To(Equal(ptr.To(fakeAzureBastionSpec1.EnableTunneling)))
-				g.Expect(result.(armnetwork.BastionHost).Tags).To(Equal(fakeBastionHostTags))
+			expect: func(g *WithT, result asonetworkv1.BastionHost) {
+				g.Expect(result).To(Not(BeNil()))
+
+				// ObjectMeta is populated later in the codeflow
+				g.Expect(result.ObjectMeta).To(Equal(metav1.ObjectMeta{}))
+
+				// Spec is populated from the spec passed in
+				g.Expect(result.Spec).To(Equal(getASOBastionHost().Spec))
 			},
-			expectedError: "",
+		},
+		{
+			name: "user updates to bastion hosts DisableCopyPaste should be accepted",
+			spec: &fakeAzureBastionSpec1,
+			existing: getASOBastionHost(
+				// user added DisableCopyPaste
+				func(bastion *asonetworkv1.BastionHost) {
+					bastion.Spec.DisableCopyPaste = ptr.To(true)
+				},
+				// user added Status
+				func(bastion *asonetworkv1.BastionHost) {
+					bastion.Status = fakeBastionHostStatus
+				},
+			),
+			expect: func(g *WithT, result asonetworkv1.BastionHost) {
+				g.Expect(result).To(Not(BeNil()))
+				resultantASOBastionHost := getASOBastionHost(
+					func(bastion *asonetworkv1.BastionHost) {
+						bastion.Spec.DisableCopyPaste = ptr.To(true)
+					},
+				)
+
+				// ObjectMeta should be carried over from existing private endpoint.
+				g.Expect(result.ObjectMeta).To(Equal(resultantASOBastionHost.ObjectMeta))
+
+				// EnableTunneling addition is accepted.
+				g.Expect(result.Spec).To(Equal(resultantASOBastionHost.Spec))
+
+				// Status should be carried over.
+				g.Expect(result.Status).To(Equal(fakeBastionHostStatus))
+			},
+		},
+		{
+			name: "user updates to ASO's bastion hosts resource and capz should overwrite it",
+			spec: &fakeAzureBastionSpec1,
+			existing: getASOBastionHost(
+				// user added DisableCopyPaste
+				func(bastion *asonetworkv1.BastionHost) {
+					bastion.Spec.DisableCopyPaste = ptr.To(true)
+				},
+
+				// user also added EnableTunneling which should be overwritten by capz
+				func(bastion *asonetworkv1.BastionHost) {
+					bastion.Spec.EnableTunneling = ptr.To(true)
+				},
+				// user added Status
+				func(bastion *asonetworkv1.BastionHost) {
+					bastion.Status = fakeBastionHostStatus
+				},
+			),
+			expect: func(g *WithT, result asonetworkv1.BastionHost) {
+				g.Expect(result).To(Not(BeNil()))
+				resultantASOBastionHost := getASOBastionHost(
+					func(endpoint *asonetworkv1.BastionHost) {
+						endpoint.Spec.DisableCopyPaste = ptr.To(true)
+					},
+				)
+
+				// user changes except DisableCopyPaste should be overwritten.
+				g.Expect(result.ObjectMeta).To(Equal(resultantASOBastionHost.ObjectMeta))
+
+				// DisableCopyPaste addition is accepted.
+				g.Expect(result.Spec).To(Equal(resultantASOBastionHost.Spec))
+
+				// Status should be carried over.
+				g.Expect(result.Status).To(Equal(fakeBastionHostStatus))
+			},
 		},
 	}
-	for _, tc := range testCases {
+
+	for _, tc := range testcases {
 		tc := tc
 		t.Run(tc.name, func(t *testing.T) {
 			g := NewWithT(t)
@@ -113,7 +195,7 @@ func TestAzureBastionSpec_Parameters(t *testing.T) {
 			} else {
 				g.Expect(err).NotTo(HaveOccurred())
 			}
-			tc.expect(g, result)
+			tc.expect(g, *result)
 		})
 	}
 }
