b9b52e54e884c4b897c6e9dc2e2c1586d64f6c1e
Daniel Lipovetsky <daniel.lipovetsky@nutanix.com>
2024-03-13 08:42:12 -0700

    fixup! Make AzureCluster subscriptionID conditionally mutable
    Fix import alias

--- api/v1beta1/azurecluster_webhook_test.go
+++ api/v1beta1/azurecluster_webhook_test.go
@@ -20,7 +20,7 @@ import (
 	"testing"
 
 	. "github.com/onsi/gomega"
-	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
@@ -262,7 +262,7 @@ func TestAzureCluster_ValidateUpdate(t *testing.T) {
 				},
 			},
 			cluster: &AzureCluster{
-				ObjectMeta: v1.ObjectMeta{
+				ObjectMeta: metav1.ObjectMeta{
 					Annotations: map[string]string{
 						AzureClusterAllowMutateSubscriptionIDAnnotation: "",
 					},
