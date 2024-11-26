7f8e6ad4d7b8cbec82df677dd12384cc4ae499d8
Nawaz Hussain Khazielakha <k.nawaz.h@gmail.com>
2024-11-26 10:51:28 -0800

    let users configure private IP of the internal LB

--- api/v1beta1/azurecluster_default.go
+++ api/v1beta1/azurecluster_default.go
@@ -20,6 +20,8 @@ import (
 	"fmt"
 
 	"k8s.io/utils/ptr"
+
+	"sigs.k8s.io/cluster-api-provider-azure/feature"
 )
 
 const (
@@ -243,6 +245,29 @@ func (c *AzureCluster) setAPIServerLBDefaults() {
 						Name: generatePublicIPName(c.ObjectMeta.Name),
 					},
 				},
+			}
+		}
+		// if the API Server ILB feature is enabled, we should create a default internal LB IP
+		if feature.Gates.Enabled(feature.APIServerILB) {
+			privateIPFound := false
+			for i := range lb.FrontendIPs {
+				if lb.FrontendIPs[i].FrontendIPClass.PrivateIPAddress != "" {
+					if lb.FrontendIPs[i].Name == "" {
+						lb.FrontendIPs[i].Name = generateFrontendIPConfigName(lb.Name) + "-internal-ip"
+					}
+					privateIPFound = true
+					break
+				}
+			}
+			// if no private IP is found, we should create a default internal LB IP
+			if !privateIPFound {
+				privateIP := FrontendIP{
+					Name: generateFrontendIPConfigName(lb.Name) + "-internal-ip",
+					FrontendIPClass: FrontendIPClass{
+						PrivateIPAddress: DefaultInternalLBIPAddress,
+					},
+				}
+				lb.FrontendIPs = append(lb.FrontendIPs, privateIP)
 			}
 		}
 	} else if lb.Type == Internal {
