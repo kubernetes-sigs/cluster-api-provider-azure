2aae8b0720f694452ca9c90399063fe290ad1c37
Matt Boersma <Matt.Boersma@microsoft.com>
2025-01-22 15:25:39 -0700

    Match the parentheses please

--- test/e2e/capi_test.go
+++ test/e2e/capi_test.go
@@ -166,7 +166,7 @@ var _ = Describe("Running the Cluster API E2E tests", func() {
 
 	Context("Should successfully set and use node drain timeout", func() {
 		// Use default as the clusterclass name so test infra can find the clusterclass template
-		Expect(os.Setenv("CLUSTER_CLASS_NAME", "ci-default").To(Succeed())
+		Expect(os.Setenv("CLUSTER_CLASS_NAME", "ci-default")).To(Succeed())
 		Expect(os.Setenv("WORKER_MACHINE_COUNT", "1")).To(Succeed())
 		capi_e2e.NodeDrainTimeoutSpec(context.TODO(), func() capi_e2e.NodeDrainTimeoutSpecInput {
 			return capi_e2e.NodeDrainTimeoutSpecInput{
