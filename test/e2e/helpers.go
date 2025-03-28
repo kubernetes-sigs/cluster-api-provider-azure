4f77fd1dc2e93cd142504c649bbe5761c376ba32
James Sturtevant <jsturtevant@gmail.com>
2025-03-28 13:55:14 -0700

    remove unused
    Signed-off-by: James Sturtevant <jsturtevant@gmail.com>

--- test/e2e/helpers.go
+++ test/e2e/helpers.go
@@ -46,7 +46,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	typedappsv1 "k8s.io/client-go/kubernetes/typed/apps/v1"
