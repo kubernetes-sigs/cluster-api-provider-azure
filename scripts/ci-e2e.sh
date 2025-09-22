a4aea5105b9af5a68f6be7ac3d2f65e770729d2a
William Yao <william2000yao@gmail.com>
2025-09-22 11:56:11 -0700

    Fix pdb flake and delete flow for aks mgmt cluster

--- scripts/ci-e2e.sh
+++ scripts/ci-e2e.sh
@@ -76,6 +76,7 @@ capz::util::generate_ssh_key
 
 capz::ci-e2e::cleanup() {
     "${REPO_ROOT}/hack/log/redact.sh" || true
+    make test-e2e-run-cleanup || true
 }
 
 trap capz::ci-e2e::cleanup EXIT
