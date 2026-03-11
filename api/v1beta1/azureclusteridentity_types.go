5303d13fe307bab0b12bbe44163982d556f7d656
Matthew Booth <mbooth@redhat.com>
2026-03-11 10:41:00 +0000

    Remove usage of controller-runtime/pkg/scheme from api
    Copies the pattern used by cluster-api.

--- api/v1beta1/azureclusteridentity_types.go
+++ api/v1beta1/azureclusteridentity_types.go
@@ -128,5 +128,5 @@ func (c *AzureClusterIdentity) SetConditions(conditions clusterv1beta1.Condition
 }
 
 func init() {
-	SchemeBuilder.Register(&AzureClusterIdentity{}, &AzureClusterIdentityList{})
+	objectTypes = append(objectTypes, &AzureClusterIdentity{}, &AzureClusterIdentityList{})
 }
