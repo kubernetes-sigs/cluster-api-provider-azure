ff0f45a0ced54f507e211cc6b977fa90c47cf824
William Yao <william2000yao@gmail.com>
2026-07-09 08:40:45 -0700

    Fix e2e test scheme: use v1beta2 AzureASO/AzureCluster types

--- test/e2e/azure_privatecluster.go
+++ test/e2e/azure_privatecluster.go
@@ -40,7 +40,7 @@ import (
 	"sigs.k8s.io/cluster-api/util"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	infrav1 "sigs.k8s.io/cluster-api-provider-azure/api/v1beta1"
+	infrav1 "sigs.k8s.io/cluster-api-provider-azure/api/v1beta2"
 	azureutil "sigs.k8s.io/cluster-api-provider-azure/util/azure"
 )
 
