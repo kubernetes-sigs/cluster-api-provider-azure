7a64d1cc3ad7234a670ddec4d1a8d8405a9146ef
Pujitha Paladugu <10557236+pujitha24@users.noreply.github.com>
2026-08-03 13:33:40 -0700

    Mark AzureMachinePool NotReady in default provisioning state case
    Restores SetNotReady() for the default case in
    setProvisioningStateAndConditions, which covers Migrating, Canceled,
    and Deleted states. This was lost in https://github.com/kubernetes-sigs/cluster-api-provider-azure/issues/5537 alongside the Failed case
    fixed in the previous commit. A Canceled VMSS provisioning state (e.g.
    an aborted scale operation) would otherwise strand the AzureMachinePool
    as Ready=true.
    
    Signed-off-by: Pujitha Paladugu <10557236+pujitha24@users.noreply.github.com>

--- azure/scope/machinepool.go
+++ azure/scope/machinepool.go
@@ -630,6 +630,7 @@ func (m *MachinePoolScope) setProvisioningStateAndConditions(v infrav1.Provision
 		m.SetNotReady()
 	default:
 		v1beta1conditions.MarkFalse(m.AzureMachinePool, infrav1.ScaleSetRunningCondition, string(v), clusterv1beta1.ConditionSeverityInfo, "")
+		m.SetNotReady()
 	}
 }
 
